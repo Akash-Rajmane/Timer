e5d2b185a4a0ddf6a8cb6faf020e9dacf9a15106
Akash-Rajmane <akashrajmane23@gmail.com>
2023-04-27 23:56:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute('r')*2*Math.PI;
+circle.setAttribute('stroke-dasharray',perimeter);
+
+let currOffset = 0;
+let duration;
+const timer = new Timer( durationInput, startBtn, pauseBtn,{
+    onStart(totalDuration){
+        duration = totalDuration;
+    },
+    onTick(timeRemaining){
+        circle.setAttribute("stroke-dashoffset",currOffset);
+        currOffset = (perimeter*timeRemaining)/duration-perimeter;
+    },
+    onComplete(){
+        console.log("Timer is completed");
+    }
+});
